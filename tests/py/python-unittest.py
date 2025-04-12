b2035c7a0d3a995f05c7bb5dd7345fcbd3279094
Cyrille Berger <cberger@cberger,net>
2025-04-12 21:12:50 +0200

    fix non-tck unittests

--- tests/py/python-unittest.py
+++ tests/py/python-unittest.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
 
 import unittest as ut
+import re
 import tempfile
 
 import gqlite
 
-class TestConnection(ut.TestCase):
-  def test_simple_queries(self):
-    fp = tempfile.NamedTemporaryFile()
-    conn = gqlite.connect(fp.name)
-    # Test one node creation
-    self.assertEqual(conn.execute_oc_query("CREATE (n)"), None)
-    # Test return
-    self.assertEqual(conn.execute_oc_query("MATCH (n) RETURN n"), [['n'], [{'properties': {}, 'id': 1, 'labels': [], 'type': 'node'}]]) 
-    # Test failure
-    with self.assertRaises(gqlite.Error) as cm:
-      conn.execute_oc_query("MATCH (n")
-    self.assertEqual(cm.exception.msg, "CompileTime: UnexpectedSyntax: Expected token ) got end of file at (1, 8).")
 
-if __name__ == '__main__':
+class TestConnection(ut.TestCase):
+    def test_simple_queries(self):
+        fp = tempfile.NamedTemporaryFile()
+        conn = gqlite.connect(fp.name)
+        # Test one node creation
+        self.assertEqual(conn.execute_oc_query("CREATE (n)"), None)
+        # Test return
+        qr = conn.execute_oc_query("MATCH (n) RETURN n")
+        del qr[1][0]["key"]
+        self.assertEqual(
+            qr, [["n"], [{"properties": {}, "labels": [], "type": "node"}]]
+        )
+        # Test failure
+        with self.assertRaises(gqlite.Error) as cm:
+            conn.execute_oc_query("MATCH (n")
+        self.assertTrue(
+            re.match("CompileTime: ParseError: .*", cm.exception.msg),
+        )
+
+
+if __name__ == "__main__":
     ut.main()
