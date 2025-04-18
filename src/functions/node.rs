47130230b3f19ed1647517bcaaa6d7d0cf816260
Cyrille Berger <cberger@cberger.net>
2025-04-18 09:55:18 +0200

    add labels

---
+++ src/functions/node.rs
@@ -0,0 +1,16 @@
+use crate::{error::RunTimeError, graph};
+
+use super::{FResult, FunctionTypeTrait};
+
+#[derive(Debug, Default)]
+pub(super) struct Labels {}
+
+impl Labels
+{
+  fn call_impl(node: &graph::Node) -> FResult<Vec<String>>
+  {
+    Ok(node.labels.to_owned())
+  }
+}
+
+super::declare_function!(labels, Labels, call_impl(crate::graph::Node) -> Vec<String>);
