93b9e11caf335ce0df367326d79018dcab255b07
Cyrille Berger <cberger@cberger.net>
2025-08-04 22:15:00 +0200

    Treat all warnings as errors on all crates.
    Should increase code quality.
    
    Issues: #56

--- bindings/python/gqlitepy/src/lib.rs
+++ bindings/python/gqlitepy/src/lib.rs
@@ -1,4 +1,4 @@
-// #![deny(warnings)]
+#![deny(warnings)]
 
 use pyo3::{
   prelude::*,
--- crates/gqlitebrowser/src/main.rs
+++ crates/gqlitebrowser/src/main.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![deny(warnings)]
 
 use gqlitedb::ValueTryIntoRef as _;
 
